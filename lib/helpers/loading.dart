34dec4ce342d060f27c8466f6e3b8f3e77446097
vinhkosd <vinh.le@robustaeng.com>
2022-04-22 15:05:52 +0700

    add sidebar to loadingpage

--- lib/helpers/loading.dart
+++ lib/helpers/loading.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/helpers/responsive.dart';
 import 'package:flutter_demo/screens/navbar/header.dart';
+import 'package:flutter_demo/screens/navbar/side_menu.dart';
 
 Scaffold loadingProcess(BuildContext context, String processText) {
   return Scaffold(
     body: SafeArea(
-        child: Column(children: [
-      Header(),
-      Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Center(
-              child: Text(
-                processText,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
-            Center(
-              child: LinearProgressIndicator(
-                semanticsLabel: 'Linear progress indicator',
+      child: Row(children: [
+        if (Responsive.isDesktop(context))
+          Expanded(
+            // default flex = 1
+            // and it takes 1/6 part of the screen
+            child: SideMenu(),
+          ),
+          Expanded(
+            // It takes 5/6 part of the screen
+            flex: 5,
+            child: Column(children: [
+              Header(),
+              Expanded(
+                child: Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        processText,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    Center(
+                      child: LinearProgressIndicator(
+                        semanticsLabel: 'Linear progress indicator',
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
-          ],
+          ]),
         ),
-      ),
-    ])),
+      ])
+    ),
   );
 }
 
